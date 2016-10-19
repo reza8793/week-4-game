9d507a0ee034e59d84fc2b2a15407d4e6be375a2
reza8793 <aabed525@gmail.com>
2016-10-19 01:44:37 -0700

    Working almost fully

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,22 @@
 
 
-	var counter = 0;
-	var jokerHealthUpated = 100;
-	var divcharacter; 
+	var counter1 = 0; // to get the number of clicks
+	var counter2 = 1; // the number of clicks for hero's health variable (see variable urCharacterHealth)
 	
-	
+
 	
 		var batman = {
-				   name: "Batman",
-				   health: 80,
-				   attackPower: 10,
-				   attackPowerIncrement: 5,
-				   image: "batman_attacking.jpg",
-				   healthIndicator: "#batman_health",
-				   totalPower: function (counter) 
+			name: "Batman",
+			health: 80,
+			attackPower: 10,
+			attackPowerIncrement: 5,
+			image: "batman_attacking.jpg",
+			healthIndicator: "#batman_health",
+			totalPower: function (counter1) 
 				   		{
-					        return (batman.attackPower+(batman.attackPowerIncrement*counter));
-					    }
-				};
+					        return (batman.attackPower+(batman.attackPowerIncrement*counter1));
+					 	}
+					};
 
 		var joker = {
 		   name: "Joker",
@@ -26,11 +25,11 @@
 		   attackPowerIncrement: 5,
 		   image: "joker.jpg",
 		   healthIndicator: "#joker_health",
-		   totalPower: function (counter) 
+		   totalPower: function (counter1) 
 				   		{
-					        return (joker.attackPower+(joker.attackPowerIncrement*counter));
+					        return (joker.attackPower+(joker.attackPowerIncrement*counter1));
 					    }
-		};
+					};
 
 		var superman = {
 		   name: "Superman",
@@ -39,11 +38,11 @@
 		   attackPowerIncrement: 5,
 		   image: "superman.jpg",
 		   healthIndicator: "#superman_health",
-		   totalPower: function (counter) 
-				   		{
-					        return (superman.attackPower+(superman.attackPowerIncrement*counter));
-					    }
-		};
+		   totalPower: function (counter1) 
+				   			{
+					        return (superman.attackPower+(superman.attackPowerIncrement*counter1));
+					   		}
+						};
 
 		var scarecrow = {
 		   name: "Scarecrow",
@@ -52,18 +51,19 @@
 		   attackPowerIncrement: 5,
 		   image: "scarecrow.jpg",
 		   healthIndicator: "#scarecrow_health",
-		   totalPower: function (counter) 
-				   		{
-					        return (scarecrow.attackPower+(scarecrow.attackPowerIncrement*counter));
-					    }
-		};
+		   totalPower: function (counter1) 
+				   			{
+					        return (scarecrow.attackPower+(scarecrow.attackPowerIncrement*counter1));
+					    	}
+						};
 
-function fighting (urCharacter, theDefender,x) 
+function fighting (urCharacter, theDefender,x,y) 
 
 	{	
 		var urCharacterTotalPower = urCharacter.totalPower(x);
 		console.log(urCharacterTotalPower);
-		var urCharacterHealth  = urCharacter.health-(x*theDefender.attackPower);
+		var urCharacterHealth  = urCharacter.health-(y*theDefender.attackPower);
+
 		var theDefenderHealth = theDefender.health-urCharacterTotalPower;
 			theDefender.health = theDefenderHealth;
 
@@ -82,41 +82,42 @@ function fighting (urCharacter, theDefender,x)
 		// console.log(theDefenderHealth);
 
 		urCharacterStatus.html("You attacked " + theDefender.name +"  for "+ urCharacterTotalPower + " damage");
-		herohealth.html(" " + urCharacterHealth +"  ");
-
-		theDefenderStatus.html(" " + theDefender.name + " attacked you for "+ theDefender.attackPower + " damage");
 		defenderhealth.html(" " + theDefenderHealth +"  ");
 
+		theDefenderStatus.html(" " + theDefender.name + " attacked you for "+ theDefender.attackPower + " damage");
+		herohealth.html(" " + urCharacterHealth +"  ");
 
-		if (urCharacterHealth <0) // lose scenario
+
+
+		if (urCharacterHealth <=0) // lose scenario
 		 {
 		   	urCharacterStatus.html("You have been defeated. GAME OVER!!! " );
 		   	theDefenderStatus.html("");		
 		   	$("#restart_button").show();	
 		 }
 
-		 if (theDefenderHealth <0)	// win scenario 
-		 {
+		 if (theDefenderHealth <=0)	// win scenario 
+		 {	
+		 	
 		   	urCharacterStatus.html("You defeated " + theDefender.name +" You can choose to fight another enemy");
 		   	theDefenderStatus.html("");	
 		   	$("#defender_div").empty();
-		   
-		   	// if ($("#enemiesAvailable").empty() == true)
-		   				
-		   	// 	{
-		   	// 		urCharacterStatus.html("You won !!! GAME OVER !!!! ");
-		   	// 		theDefenderStatus.html("");	
-		   	// 		$("#restart_button").show();
-		   	// 	}			
+
+		   	// after hero wins one game, he needs to defeat two more chars
+		   	// setting counter2 to zero for those cases
+		   	 counter2 = 0;
+		   	 urCharacter.health = urCharacterHealth;
+		   		
+		   		if(  $('#enemiesAvailable div').length === 0 ) 
+		   		{		
+		   			
+		   			urCharacterStatus.html("You won !!! GAME OVER !!!! ");
+		   			theDefenderStatus.html("");	
+		   			$("#restart_button").show();
+		   		}			
 		 }
 
-		   // 			if (newHealthBatman < 0 )
-		   // 			{
-		   // 				$("#yourCharacterFight").html("You have been defeated. GAME OVER!!! " );
-		   // 				$("#enemyCharacterFight").html(" ");
-
-
-
+		   
 	}
 
 
@@ -124,16 +125,13 @@ function fighting (urCharacter, theDefender,x)
 	{
 
 		a.health = 80;
-		a.totalPower(e);
-
+		
 		b.health = 100;
-		b.totalPower(e);
-
+		
 		c.health = 150;
-		c.totalPower(e);
-
+		
 		d.health = 60;
-		d.totalPower(e);
+		//d.totalPower = d.totalPower(e);
 	}
 
 	
@@ -147,9 +145,6 @@ function fighting (urCharacter, theDefender,x)
 		function restart_game()
 
 		{
-			alert("i'm at restart function!");
-
-			
 			// reset back the divs 
 			$("#initialCharacters").append(x[0]);
 			$("#initialCharacters").append(x[1]);
@@ -166,6 +161,10 @@ function fighting (urCharacter, theDefender,x)
 
 
 			 resetHealth(batman,joker,superman,scarecrow,0);
+			 //console.log(resetHealth);
+			 counter1 = 0;
+			 counter2 = 1;
+			 console.log(batman);
 
 			// initiate variables
 			 $("#batman_health").html(" " + batman.health +"  ");
@@ -245,155 +244,112 @@ function fighting (urCharacter, theDefender,x)
 
 		  	{ 	
 
+		  		if(  $('#defender_div div').length === 0 ) 
+		   		{		
+		   			$("#yourCharacterFight").html("No enemy here ");
+					$("#enemyCharacterFight").html("");
+
+		   		}	
+
 		 	 if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-1' && ($("#defender_div div").attr('id')) ==='characterDiv-2' )
 		 		{	
 		 		// batman vs joker 
-		 		fighting (batman, joker,counter);
-		 		counter++;
+		 		fighting (batman, joker,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 		}
 
 		 	 if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-1' && ($("#defender_div div").attr('id')) ==='characterDiv-3' )
 
 		 	 	{	// batman vs superman 
-		 	 	fighting (batman, superman,counter);
-		 		counter++;
+		 	 	fighting (batman, superman,counter1,counter2);
+		 		counter1++;
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-1' && ($("#defender_div div").attr('id')) ==='characterDiv-4' )
 		 		{
 		 		// batman vs scarecrow 
-		 		fighting (batman, scarecrow,counter);
-		 		counter++;
+		 		fighting (batman, scarecrow,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 		}
 
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-2'&& ($("#defender_div div").attr('id')) ==='characterDiv-1' )
 
 		 		{// joker vs batman
-		 		fighting (joker, batman,counter);
-		 		counter++;
+		 		fighting (joker, batman,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 	
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-2' && ($("#defender_div div").attr('id')) ==='characterDiv-3' )
 
 		 		{// joker vs superman
-		 		fighting (joker, superman,counter);
-		 		counter++;
+		 		fighting (joker, superman,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-2'&& ($("#defender_div div").attr('id')) ==='characterDiv-4' )
 
 		 		{// joker vs scarecrow
-		 		fighting (joker, scarecrow,counter);
-		 		counter++;
+		 		fighting (joker, scarecrow,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 	
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-3' && ($("#defender_div div").attr('id')) ==='characterDiv-1' )
 
 		 		{// superman vs batman
-		 		fighting (superman, batman,counter);
-		 		counter++;
+		 		fighting (superman, batman,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-3'&& ($("#defender_div div").attr('id')) ==='characterDiv-2' )
 
 		 		{// superman vs joker
-		 		fighting (superman, joker,counter);
-		 		counter++;
+		 		fighting (superman, joker,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-3'&& ($("#defender_div div").attr('id')) ==='characterDiv-4' )
 
 		 		{// superman vs scarecrow
-		 		fighting (superman, scarecrow,counter);
-		 		counter++;
-		 	
+		 		fighting (superman, scarecrow,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-4' && ($("#defender_div div").attr('id')) ==='characterDiv-1' )
 
 		 		{// scarecrow vs batman
-		 		fighting (scarecrow, batman,counter);
-		 		counter++;
+		 		fighting (scarecrow, batman,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 	
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-4' && ($("#defender_div div").attr('id')) ==='characterDiv-2' )
 
 		 		{// scarecrow vs joker
-		 		fighting (scarecrow, joker,counter);
-		 		counter++;
+		 		fighting (scarecrow, joker,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 		}
 
 		 	if ( ($("#yourCharacter div").attr('id')) === 'characterDiv-4' && ($("#defender_div div").attr('id')) ==='characterDiv-3' )
 
 		 		{// scarecrow vs superman
-		 		fighting (scarecrow, superman,counter);
-		 		counter++;
+		 		fighting (scarecrow, superman,counter1,counter2);
+		 		counter1++;
+		 		counter2++;
 		 	
 		 		}
-
-
-		  	// if (x[1].id == 'characterDiv-2' && divIndex === 0)
-
-	  		// {
-		  	// 	 var batmanTotalPower = batman.totalPower(counter);
-		  		
-		  	// 	 console.log("total Power "+ batmanTotalPower);
-
-		  	// 	 var newHealthBatman  = batman.health-(counter*joker.attackPower);
-
-		  	// 	 console.log("joker Health  "+ joker.health);
-
-
-		  	// 	 var newHealthJoker = jokerHealthUpated-batmanTotalPower;
-
-		  	// 	 jokerHealthUpated = newHealthJoker;
-
-
-		  	// 	if (counter === 0) // for initial attack 
-		  	// 	{	
-		  	// 		var urChar = $("#yourCharacterFight");
-		  	// 		batman.whateverUWant = $("#yourCharacterFight");
-
-		  	// 		batman.whateverUWant.html("You attacked " + joker.name +"  for "+ batmanTotalPower + " damage");
-		   // 			$("#enemyCharacterFight").html(" " + joker.name + " attacked you for "+ joker.attackPower + " damage");
-
-		  	// 	}
-
-		  	// 	if (counter > 0) // for cases when attacked more than once 
-		  	// 	{
-
-
-		   // 			$("#yourCharacterFight").html("You attacked " + joker.name +"  for "+ batmanTotalPower +" damage");
-		   // 			$("#batman_health").html(" " + newHealthBatman +"  ");
-
-		   // 			$("#enemyCharacterFight").html(" " + joker.name + " attacked you for "+ joker.attackPower+" damage");
-		   // 			$("#joker_health").html(" " + newHealthJoker +"  ");
-
-		   // 			if (newHealthJoker < 0 )
-		   // 			{
-		   // 				$("#yourCharacterFight").html("You defeated " + joker.name +
-		   // 					" You can choose to fight another enemy");
-		   // 				$("#enemyCharacterFight").html(" ");
-		   				
-		   // 			}
-
-		   // 			if (newHealthBatman < 0 )
-		   // 			{
-		   // 				$("#yourCharacterFight").html("You have been defeated. GAME OVER!!! " );
-		   // 				$("#enemyCharacterFight").html(" ");
-
-		   // 			}
-		  			
-		  	// 	}
-
-		  	// 	counter++;
-
-		  	// }
-
 		  });
 
 
@@ -402,11 +358,8 @@ function fighting (urCharacter, theDefender,x)
 		   $("#restart_button").on("click", function()
 
 		  	{ 	
-		  		alert("i'm at restart button!");
 		  		restart_game();
 
 		  	});
-
-
 
 	});
